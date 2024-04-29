ca3c74cdfc0f9328080fb2e5f783f69ff34c5b74
Raushan Kumar <raushan911721@gmail.com>
2024-04-30 00:24:50 +0530

    redux-toolkit-concepts

---
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+function Todos() {
+  const todos = useSelector(state => state.todos)
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeTodo(todo.id))} className='m-2'>X</button>
+        </li>
+      ))}
+    </>
+  )
+}
+
+export default Todos
